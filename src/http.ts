34d359337fb0e28a725fe9beefa876218c9a5b59
curse <58047023+ethndotsh@users.noreply.github.com>
2023-09-27 13:26:48 -0600

    whoops x3

--- src/http.ts
+++ src/http.ts
@@ -1,9 +1,9 @@
 async function request(url: string, options: RequestInit) {
   const res = await fetch(url, options);
-  const body = await res.json();
+
   return {
     headers: res.headers,
-    body,
+    body: Buffer.from(await res.arrayBuffer()),
   };
 }
 
