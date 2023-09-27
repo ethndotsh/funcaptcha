13fd91660e3c591536b0d5dc71b6c6b1b9d3e379
curse <58047023+ethndotsh@users.noreply.github.com>
2023-09-27 11:56:25 -0600

    whoops one more time

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-export * from "./api";
-export * from "./session";
+import { Session, type TokenInfo, type SessionOptions } from "./session";
+import { getToken, type GetTokenOptions, type GetTokenResult } from "./api";
+
+export default {
+  Session,
+  getToken,
+};
+
+export type { TokenInfo, SessionOptions, GetTokenOptions, GetTokenResult };
